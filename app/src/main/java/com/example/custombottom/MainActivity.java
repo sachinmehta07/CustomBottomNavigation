7f11550b030729d1da42c89b92e320ffc6417edf
sachinmehta07 <mehtasachin53922@gmail.com>
2023-07-16 15:30:15 +0530

    custom dialog_custom.xml

--- app/src/main/java/com/example/custombottom/MainActivity.java
+++ app/src/main/java/com/example/custombottom/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.custombottom;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton actionButton = findViewById(R.id.floatingActionButton);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+    }
+
+    private void showCustomDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_custom, null);
+        builder.setView(dialogView);
+
+        TextView dialogTitle = dialogView.findViewById(R.id.dialog_title);
+        Button dialogYesButton = dialogView.findViewById(R.id.dialog_yes);
+        Button dialogNoButton = dialogView.findViewById(R.id.dialog_no);
+
+        dialogYesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle 'Yes' button click
+                // Perform your desired action
+                // For example, finish the activity to exit
+                finish();
+            }
+        });
+
+        dialogNoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle 'No' button click
+                // Dismiss the dialog
+                //dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
